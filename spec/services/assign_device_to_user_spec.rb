ac40c0f2bd5f23c794d4d04bc04a1b5fc98055c8
DrArzter <chapegarostislav@gmail.com>
2025-07-04 12:46:12 +0200

    Add specs for AssignDeviceToUser service
    Add comprehensive tests covering edge cases like already assigned devices and prevent re-assignment of returned devices.

--- spec/services/assign_device_to_user_spec.rb
+++ spec/services/assign_device_to_user_spec.rb
@@ -3,59 +3,80 @@
 require 'rails_helper'
 
 RSpec.describe AssignDeviceToUser do
-  subject(:assign_device) do
+  let(:result_struct) { Struct.new(:success?, :data, :error) }
+
+  let!(:user) { create(:user) }
+  let!(:device) { create(:device) }
+
+  subject(:call_service) do
     described_class.new(
       requesting_user: user,
-      serial_number: serial_number,
-      new_device_owner_id: new_device_owner_id
+      serial_number: device.serial_number
     ).call
   end
 
-  let(:user) { create(:user) }
-  let(:serial_number) { '123456' }
+  context 'when device is available and unassigned' do
+    it 'assigns the device to the user' do
+      expect { call_service }.to change(DeviceAssignment, :count).by(1)
+    end
 
-  context 'when users registers a device to other user' do
-    let(:new_device_owner_id) { create(:user).id }
+    it 'returns a successful result object' do
+      result = call_service
+      expect(result).to be_success
+      expect(result.error).to be_nil
+      expect(result.data).to be_a(DeviceAssignment)
+    end
 
-    it 'raises an error' do
-      expect { assign_device }.to raise_error(RegistrationError::Unauthorized)
+    it 'associates the assignment with the correct user and device' do
+      call_service
+      assignment = DeviceAssignment.last
+      expect(assignment.user).to eq(user)
+      expect(assignment.device).to eq(device)
+      expect(assignment.active).to be(true)
     end
   end
 
-  context 'when user registers a device on self' do
-    let(:new_device_owner_id) { user.id }
+  context 'when device is already assigned to another user' do
+    let!(:another_user) { create(:user) }
+    before { create(:device_assignment, device: device, user: another_user) }
 
-    it 'creates a new device' do
-      assign_device
-
-      expect(user.devices.pluck(:serial_number)).to include(serial_number)
+    it 'does not create a new assignment' do
+      expect { call_service }.not_to change(DeviceAssignment, :count)
     end
 
-    context 'when a user tries to register a device that was already assigned to and returned by the same user' do
-      before do
-        assign_device
-        ReturnDeviceFromUser.new(user: user, serial_number: serial_number, from_user: user.id).call
-      end
+    it 'returns a failure result with an error code' do
+      result = call_service
+      expect(result).not_to be_success
+      expect(result.error).to eq(:device_already_assigned)
+    end
+  end
 
-      it 'does not allow to register' do
-        expect { assign_device }.to raise_error(AssigningError::AlreadyUsedOnUser)
-      end
+  context 'when user tries to re-assign a device they previously returned' do
+    before { create(:device_assignment, :returned, device: device, user: user) }
+
+    it 'does not create a new assignment' do
+      expect { call_service }.not_to change(DeviceAssignment, :count)
     end
 
-    context 'when user tries to register device that is already assigned to other user' do
-      let(:other_user) { create(:user) }
+    it 'returns a failure result with an error code' do
+      result = call_service
+      expect(result).not_to be_success
+      expect(result.error).to eq(:reassign_forbidden)
+    end
+  end
 
-      before do
-        AssignDeviceToUser.new(
-          requesting_user: other_user,
-          serial_number: serial_number,
-          new_device_owner_id: other_user.id
-        ).call
-      end
+  context 'when device with given serial number does not exist' do
+    subject(:call_service) do
+      described_class.new(
+        requesting_user: user,
+        serial_number: 'NON-EXISTENT-SN'
+      ).call
+    end
 
-      it 'does not allow to register' do
-        expect { assign_device }.to raise_error(AssigningError::AlreadyUsedOnOtherUser)
-      end
+    it 'returns a failure result' do
+      result = call_service
+      expect(result).not_to be_success
+      expect(result.error).to eq(:device_not_found)
     end
   end
 end
